3ed286ce924477609538750c5c6d7598009660f1
cagla.sen <cagla.sen@tubitak.gov.tr>
2022-03-04 11:13:16 +0300

    add file SQL HW2

---
+++ Week 1 - SQL/SQL_HW2.sql
@@ -0,0 +1,11 @@
+-- Author: Çağla Şen
+--Odev 2
+-- 1
+SELECT * from film WHERE film.replacement_cost BETWEEN 12.99 AND 16.99;
+
+--2
+SELECT first_name, last_name  FROM actor WHERE actor.first_name IN('Penelope', 'Nick', 'Ed');
+
+--3
+SELECT * FROM film WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
+
